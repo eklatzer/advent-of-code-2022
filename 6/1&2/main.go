06b9cc5db663b9526fc0c03172d027c72b6ed5d3
Eric Klatzer <eric@klatzer.at>
2022-12-06 16:43:18 +0100

    adjusted flag handling

--- 6/1&2/main.go
+++ 6/1&2/main.go
@@ -7,15 +7,19 @@ import (
 	"advent-of-code-2022/6/util"
 )
 
-func main() {
-	numberOfDistinctCharsForMarker := flag.Int("n", 4, "Number of distinct chars in a row for a marker")
-	flag.Parse()
+var numberOfDistinctCharsForMarker int
 
+func init() {
+	flag.IntVar(&numberOfDistinctCharsForMarker, "n", 4, "Number of distinct chars in a row for a marker")
+	flag.Parse()
+}
+
+func main() {
 	line := util.GetInputLine()
 	for i := 0; i < len(line); i++ {
-		elements := util.GetNValuesStartingFromIndex(line, i, *numberOfDistinctCharsForMarker)
-		if util.GetNumberOfDifferentChars(elements) == *numberOfDistinctCharsForMarker {
-			log.Printf("index after start of marker: %d", (i + *numberOfDistinctCharsForMarker))
+		elements := util.GetNValuesStartingFromIndex(line, i, numberOfDistinctCharsForMarker)
+		if util.GetNumberOfDifferentChars(elements) == numberOfDistinctCharsForMarker {
+			log.Printf("index after start of marker: %d", (i + numberOfDistinctCharsForMarker))
 			break
 		}
 	}
